6760e6d0f5cc2d8c02345b6fada962b4be1c94ea
Shashwat Kadam <shashwatkdm@gmail.com>
2019-08-18 13:10:39 +0530

    Completed code for both functions

--- Assignment_1/code_files/q3.py
+++ Assignment_1/code_files/q3.py
@@ -9,22 +9,45 @@ import time
 #       has to be updated, because it's producing error (Unsupported datatype)     
 
 spine_image = cv2.imread(sys.argv[1], 0)
-print(spine_image[500:600, 500:600])
-cv2.imshow('Original Spine Image', spine_image[500:600, 500:600])
+#print(spine_image)
+cv2.imshow('Original Spine Image', spine_image)
 cv2.waitKey(0)
 
-def log_transformation(image, c):
+def log_transformation(image):
+    '''
     transformed_image = np.zeros(image.shape, dtype='float16')
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             transformed_image[i][j] = c * math.log(image[i,j] + 1.)
+    '''
+    c = np.ceil(255 / (np.log(1 + np.max(image))))
+    log_image = c * (np.log(image + 1))
+    log_image = np.array(log_image, dtype=np.uint8)
+    # print('MIN {} MAX {}'.format(transformed_image.min(), transformed_image.max()))        
+    # print(log_image[500:600, 500:600], '\nMAX{} MIN{}'.format(log_image.max(), log_image.min()))
+    figure = plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(image, cmap='gray')
 
-    print('MIN {} MAX {}'.format(transformed_image.min(), transformed_image.max()))        
-    cv2.imshow('transformed image', transformed_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    plt.subplot(1,2,2)
+    plt.imshow(log_image, cmap='gray', vmin=log_image.min(), vmax=log_image.max())
+    
+    plt.show()
     
 def power_transformation(image, c, gamma):
-    pass
+    spine_gamma = np.array(255 * (image/255)**gamma, dtype=np.uint8)
+    
+    figure = plt.figure()
 
-log_transformation(spine_image, 1)
+    plt.subplot(1,2,1)
+    plt.imshow(image, cmap='gray')
+
+    plt.subplot(1,2,2)
+    plt.imshow(spine_gamma, cmap='gray')
+
+    plt.show()
+
+log_transformation(spine_image)
+
+power_transformation(spine_image, 1, 2.2)
+power_transformation(spine_image, 1, 0.4)
