844d8e4d9d9ae8f7f6eb838a036e937c5f7b001c
Shashwat Kadam <shashwatkdm@gmail.com>
2019-09-10 18:44:18 +0530

    Padding function modified to pad according to unqual height and width of filter

--- Assignment_2/code_files/q2.py
+++ Assignment_2/code_files/q2.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sys import argv
 
-image = cv2.imread(argv[1], 0)
-
 # TODO: Verify the results
 
 filters = {
@@ -15,16 +13,19 @@ filters = {
     'roberts_h': np.array([[0,1,0],[-1,0,0],[0,0,0]]),
     'roberts_v': np.array([[1,0,0],[0,-1,0],[0,0,0]]),
     'laplacian': np.array([[0,-1,0],[-1,4,-1],[0,-1,0]]),
-    'laplacian_d': np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    'laplacian_d': np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]),
+    'averaging': 0.04*np.ones(shape=(5,5))
 }
 
-def padding(image, filter_size):
+def padding(image, filter_shape):
     height, width = image.shape 
-    padding_thickness = filter_size//2
+    filter_height, filter_width = filter_shape
+    vertical_padding_thickness = filter_height//2
+    horizontal_padding_thickness = filter_width//2
     # vertically and horizontally stack 0 arrays
-    hzeros = np.zeros(shape=(height, padding_thickness))
+    hzeros = np.zeros(shape=(height, horizontal_padding_thickness))
     image = np.hstack((hzeros, image, hzeros))
-    vzeros = np.zeros(shape=(padding_thickness, width+2*padding_thickness))
+    vzeros = np.zeros(shape=(vertical_padding_thickness, width+2*horizontal_padding_thickness))
     image = np.vstack((vzeros, image, vzeros))
     return image
 
@@ -34,14 +35,16 @@ def round_off(image):
 def apply_filter(image, _filter, filter_name):
     image_after_filtering = np.zeros(shape=image.shape)
     filter_size = len(_filter)
+    filter_height, filter_width = _filter.shape 
     initial_height, initial_width = image.shape
-    image = padding(image, filter_size)
+    image = padding(image, _filter.shape)
     height, width = image.shape
-    for x in range(height-filter_size):
-        for y in range(width-filter_size):
-            correlation_value = np.dot(image[x:x+filter_size, y:y+filter_size].flatten(), _filter.flatten())
+    for x in range(height-filter_height):
+        for y in range(width-filter_width):
+            correlation_value = np.dot(image[x:x+filter_height, y:y+filter_width].flatten(), _filter.flatten())
             image_after_filtering[x][y] = correlation_value
     image_after_filtering = round_off(image_after_filtering)
+    # image_after_filtering = np.absolute(image_after_filtering)
     plt.subplot(1,2,1)
     plt.imshow(image_after_filtering, cmap='gray')
     plt.title('Filter applied ' + filter_name)
@@ -49,7 +52,9 @@ def apply_filter(image, _filter, filter_name):
     plt.imshow(image, cmap='gray')
     plt.title('Original image')
     plt.show()
-    
-figure = plt.figure()
-for (key, value) in filters.items():
-    apply_filter(image, value, key)
+
+if __name__ == '__main__':  
+    image = cv2.imread(argv[1], 0)  
+    figure = plt.figure()
+    for (key, value) in filters.items():
+        apply_filter(image, value, key)
--- Assignment_2/code_files/q3.py
+++ Assignment_2/code_files/q3.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
+from q2 import padding
 
 # TODO: Image quality is not as expected
 
@@ -8,68 +9,67 @@ image = cv2.imread('../input_images/skeleton_orig.tif', 0)
 height, width = image.shape
 
 filters = {
-    'Laplacian' : np.array([[0,-1,0],[-1,4,1],[0,-1,0]]),
-    'Sobel_h' : 0.125*np.array([[-1,0,1],[-2,0,2],[-1,0,1]]),
-    'Sobel_v' : 0.125*np.array([[1,2,1],[0,0,0],[-1,-2,-1]]),
-    'Averaging': 0.04*np.ones(shape=(5,5))
+    'laplacian' : np.array([[0,-1,0],[-1,4,1],[0,-1,0]]),
+    'sobel_h' : 0.125*np.array([[-1,0,1],[-2,0,2],[-1,0,1]]),
+    'sobel_v' : 0.125*np.array([[1,2,1],[0,0,0],[-1,-2,-1]]),
+    'averaging': 0.04*np.ones(shape=(5,5))
 }
 
-def padding(image, filter_size):
-    height, width = image.shape 
-    padding_thickness = filter_size//2
-    # vertically and horizontally stack 0 arrays
-    hzeros = np.zeros(shape=(height, padding_thickness))
-    image = np.hstack((hzeros, image, hzeros))
-    vzeros = np.zeros(shape=(padding_thickness, width+2*padding_thickness))
-    image = np.vstack((vzeros, image, vzeros))
-    return image
+
+def dot_product(a, b):
+    length = len(a)
+    s = 0
+    for i in range(length):
+        s += np.dot(a[i], b[i])
+    return s     
 
 def apply_filter(image, _filter, filter_name=None):
     image_after_filtering = np.zeros(shape=image.shape)
     filter_size = len(_filter)
     initial_height, initial_width = image.shape
-    image = padding(image, filter_size)
+    image = padding(image, _filter.shape)
+    filter_height, filter_width = _filter.shape
     height, width = image.shape
-    for x in range(height-filter_size):
-        for y in range(width-filter_size):
-            correlation_value = np.dot(image[x:x+filter_size, y:y+filter_size].flatten(), _filter.flatten())
+    for x in range(height-filter_height):
+        for y in range(width-filter_width):
+            correlation_value = dot_product(image[x:x+filter_height, y:y+filter_width], _filter)
             image_after_filtering[x][y] = correlation_value
     return image_after_filtering 
 
 def power_law(image, gamma):
-    s = np.array(255*(image/255)**gamma)
+    s = np.array(image**gamma)
     return s
 
-# Step 1: APPLY LAPLACIAN FILTER
-print("Step 1: Applying Laplacian filter")
-laplacian_filtered_image = apply_filter(image, filters['Laplacian'])
-# Step 2: SUBTRACT THE FILTERED IMAGE FROM ORIGINAL IMAGE
-print("Step 2: Image sharpening")
-sharpened_image = np.absolute(image - laplacian_filtered_image)
-# Step 3: APPLY SOBEL FILTERS (VERTICAL AND HORIZONTAL) AND ADD THEM
-print("Step 3: Applying Sobel filters")
-sobel_filtered_image = np.absolute(apply_filter(sharpened_image, filters['Sobel_h']) + apply_filter(image, filters['Sobel_v']))
-# Step 4: APPLY AVERAGING FILTER TO SMOOTH THE IMAGE
-print("Step 4: Applying Averaging filter")
-smooth_image = apply_filter(sobel_filtered_image, filters['Averaging'])
-# Step 5: DO PRODUCT OF LAPLACIAN FILTERED AND SMOOTHED IMAGE
-print("Step 5: Product")
-product_image = laplacian_filtered_image * smooth_image
-# Step 6: ADD THE PRODUCT IMAGE TO ORIGINAL IMAGE
-print("Step 6: Sum")
-sum_image = product_image + image 
-'''
-# Step 7: APPLY POWER LAW TRANSFORM WITH GAMMA < 1, e.g. 0.5
-print('Step 7: Power law transform (0.5)')
-transformed_image = power_law(sum_image, 0.5)
-'''
+if __name__ == '__main__':
+    # Step 1: APPLY LAPLACIAN FILTER
+    print("Step 1: Applying Laplacian filter")
+    laplacian_filtered_image = apply_filter(image, filters['laplacian'])
+    # Step 2: SUBTRACT THE FILTERED IMAGE FROM ORIGINAL IMAGE
+    print("Step 2: Image sharpening")
+    sharpened_image = np.absolute(image - laplacian_filtered_image)
+    # Step 3: APPLY SOBEL FILTERS (VERTICAL AND HORIZONTAL) AND ADD THEM
+    print("Step 3: Applying Sobel filters")
+    sobel_filtered_image = np.absolute(apply_filter(sharpened_image, filters['sobel_h']) + apply_filter(image, filters['sobel_v']))
+    # Step 4: APPLY AVERAGING FILTER TO SMOOTH THE IMAGE
+    print("Step 4: Applying Averaging filter")
+    smooth_image = apply_filter(sobel_filtered_image, filters['averaging'])
+    # Step 5: DO PRODUCT OF LAPLACIAN FILTERED AND SMOOTHED IMAGE
+    print("Step 5: Product")
+    product_image = laplacian_filtered_image * smooth_image
+    # Step 6: ADD THE PRODUCT IMAGE TO ORIGINAL IMAGE
+    print("Step 6: Sum")
+    sum_image = product_image + image
+    '''
+    # Step 7: APPLY POWER LAW TRANSFORM WITH GAMMA < 1, e.g. 0.5
+    print('Step 7: Power law transform (0.5)')
+    transformed_image = power_law(sum_image, 0.5)
+    '''
+    figure = plt.figure()
 
-figure = plt.figure()
-
-plt.subplot(1,2,1)
-plt.imshow(image, cmap='gray')
-plt.title('Original image')
-plt.subplot(1,2,2)
-plt.imshow(transformed_image, cmap='gray')
-plt.title('Transformed image')
-plt.show()
+    plt.subplot(1,2,1)
+    plt.imshow(image, cmap='gray')
+    plt.title('Original image')
+    plt.subplot(1,2,2)
+    plt.imshow(sum_image, cmap='gray')
+    plt.title('Transformed image')
+    plt.show()
