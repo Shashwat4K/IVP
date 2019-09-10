a54823be934e969b1a7518d3a8ea9927da576303
Shashwat Kadam <shashwatkdm@gmail.com>
2019-09-10 18:45:19 +0530

    Template matching code

---
+++ Assignment_2/code_files/q1.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt
+from sys import argv 
+from q2 import padding
+from q3 import dot_product
+
+image = cv2.imread(argv[1], 0)
+template = cv2.imread(argv[2], 0)
+
+figure = plt.figure()
+
+def template_matching(image, template):
+    template_height, template_width = template.shape
+    template_matching_map = np.zeros(shape=image.shape)
+    padded_image = padding(image, template.shape)
+    template_height, template_width = template.shape
+    height, width = padded_image.shape
+    for x in range(height-template_height):
+        for y in range(width-template_width):
+            t = dot_product(padded_image[x:x+template_height, y:y+template_width], template)
+            template_matching_map[x,y] = t
+    plt.subplot(1,3,1)
+    plt.imshow(image, cmap='gray')
+    plt.title('Image')
+    plt.subplot(1,3,2)
+    plt.imshow(template, cmap='gray')
+    plt.title('Template')
+    plt.subplot(1,3,3)
+    plt.imshow(template_matching_map, cmap='gray')
+    plt.title('Template matching map')
+    plt.show()
+
+template_matching(image, template)    
